a234207e735321628cc53767a5fcd5294df5772a
moniratna <moniratna.munshi123@gmail.com>
2025-07-31 16:35:34 +0530

    fix: refetched added

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -49,13 +49,13 @@ const MainPage: React.FC = () => {
 			? true
 			: false
 	);
-	// const { data: agentMessage, refetch: refetchAgentMessage } =
-	// 	useGEtAgentMessages(
-	// 		agentType,
-	// 		localStorage.getItem("authToken") || "",
-	// 		agentType !== "" && agentType !== null ? true : false
-	// 	);
-	// const threadMessages = agentMessage?.pages.flatMap((page) => page.messages);
+	const { data: agentMessage, refetch: refetchAgentMessage } =
+		useGEtAgentMessages(
+			agentType,
+			localStorage.getItem("authToken") || "",
+			agentType !== "" && agentType !== null ? true : false
+		);
+	const threadMessages = agentMessage?.pages.flatMap((page) => page.messages);
 	const handleNewChat = () => {
 		const newChat: ChatThread = {
 			id: "new",
@@ -105,13 +105,13 @@ const MainPage: React.FC = () => {
 	// 	}
 	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	// }, [autoSearch, searchQuery]);
-	// useEffect(() => {
-	// 	if (threadMessages) {
-	// 		if (allChats.length === 0) {
-	// 			setAllChats([...threadMessages]);
-	// 		}
-	// 	}
-	// }, [agentMessage]);
+	useEffect(() => {
+		if (threadMessages) {
+			if (allChats.length === 0) {
+				setAllChats([...threadMessages]);
+			}
+		}
+	}, [agentMessage]);
 	const { mutate: chatAgent } = useChatAgent();
 	const { refetch: refetchThreads } = useThreads(
 		localStorage.getItem("authToken") || "",
@@ -258,6 +258,7 @@ const MainPage: React.FC = () => {
 					setHasFetchedOnce(true);
 					setUserPrompt(null);
 					setTriggerPrompt(false);
+					refetchAgentMessage();
 				},
 				onError: () => {
 					setMessageRetry(userMessage.id);
